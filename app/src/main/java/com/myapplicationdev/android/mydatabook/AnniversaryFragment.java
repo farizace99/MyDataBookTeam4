56e6b09038589d5fc8f73c9b2710d6dde0849540
19047433 <19047433@myrp.edu.sg>
2021-06-03 13:28:48 +0800

    Nicole Annixml and anniJava button.

--- app/src/main/java/com/myapplicationdev/android/mydatabook/AnniversaryFragment.java
+++ app/src/main/java/com/myapplicationdev/android/mydatabook/AnniversaryFragment.java
@@ -7,6 +7,8 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -14,7 +16,8 @@ import android.view.ViewGroup;
  * create an instance of this fragment.
  */
 public class AnniversaryFragment extends Fragment {
-
+    Button btnEdit;
+    TextView textView;
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
@@ -59,6 +62,16 @@ public class AnniversaryFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_anniversary, container, false);
+        View view = inflater.inflate(R.layout.fragment_anniversary, container, false);
+        btnEdit = view.findViewById(R.id.btnEditAnni);
+        textView = view.findViewById(R.id.tvAnni);
+
+        btnEdit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                
+            }
+        });
+        return view;
     }
 }
